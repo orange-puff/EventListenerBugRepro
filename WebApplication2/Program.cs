5b0c2f2bd933c8039f0a3ab3237724ed197ece12
John Mancini <john.mancini@thetradedesk.com>
2025-06-20 15:34:57 -0700

    first commit

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Encoding = System.Text.Encoding;
+
+namespace WebApplication2
+{
+    public class CountersEventListener : EventListener
+    {
+        private readonly List<string> TrackedEvents = new List<string>
+        {
+            "Microsoft.AspNetCore.Hosting",
+            "Microsoft-AspNetCore-Server-Kestrel",
+        };
+
+        public readonly ConcurrentDictionary<string, EventSource> EventSources = new ConcurrentDictionary<string, EventSource>();
+        private string Name;
+
+        public CountersEventListener( string name )
+        {
+            this.Name = name;
+            EventSourceCreated += OnEventSourceCreated;
+        }
+
+        private void Log( string message )
+        {
+            Console.WriteLine( $"{DateTime.UtcNow.ToString( "yyyy-MM-dd HH:mm:ss.fff" )}:{Name}: {message}" );
+        }
+
+        private void OnEventSourceCreated( object? sender, EventSourceCreatedEventArgs e )
+        {
+            if ( e.EventSource == null || !TrackedEvents.Contains( e.EventSource.Name ) )
+            {
+                return;
+            }
+
+            this.Log( $"{e.EventSource.ToString()} has been created" );
+            EventSources[ e.EventSource.Name ] = e.EventSource;
+        }
+
+        protected override void OnEventWritten( EventWrittenEventArgs eventData )
+        {
+            if ( eventData.EventName is null || !eventData.EventName.Equals( "EventCounters" ) || eventData.Payload == null )
+            {
+                return;
+            }
+
+            if ( !this.TrackedEvents.Contains( eventData.EventSource.Name ) )
+            {
+                return;
+            }
+
+            this.Log( $"{eventData.EventSource} event has been written" );
+        }
+
+        public void DisableEvents()
+        {
+            foreach ( var keyValuePair in EventSources )
+            {
+                DisableEvents( keyValuePair.Value );
+            }
+
+            this.Log( $"Disabled all events {string.Join( ",", EventSources.Keys )}" );
+        }
+
+
+        /// <summary>
+        /// invoke CountersEventListener.Instance.EnableEvents
+        /// </summary>
+        public void EnableEvents()
+        {
+            var args = new Dictionary<string, string?>
+            {
+                [ "EventCounterIntervalSec" ] = "1"
+            };
+
+            foreach ( var keyValuePair in EventSources )
+            {
+                EnableEvents( keyValuePair.Value, EventLevel.Verbose, EventKeywords.All, args );
+            }
+
+            this.Log( $"Enabled all events {string.Join( ",", EventSources.Keys )}" );
+        }
+    }
+
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            var builder = WebApplication.CreateBuilder( args );
+            var app = builder.Build();
+            app.Map( new PathString( "/myhandler" ), (Action<IApplicationBuilder>)( b => b.UseMyHandler() ) );
+            var timeToEnable = TimeSpan.FromSeconds( 10 );
+            var timeToDisable = timeToEnable.Multiply( 2 );
+
+            var c3 = new CountersEventListener( "c3" );
+            Task.Run(
+                () =>
+                {
+                    Thread.Sleep( timeToEnable );
+                    c3.EnableEvents();
+                } );
+
+            var c2 = new CountersEventListener( "c2" );
+            Task.Run(
+                () =>
+                {
+                    Thread.Sleep( timeToDisable );
+                    c2.DisableEvents();
+                } );
+
+
+            app.Run();
+        }
+    }
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyHandler( this IApplicationBuilder builder )
+        {
+            return builder.UseMiddleware<MyHandler>();
+        }
+    }
+
+    public class MyHandler
+    {
+        public MyHandler( RequestDelegate next )
+        {
+        }
+
+        public async Task Invoke( HttpContext context )
+        {
+            await context.Response.Body.WriteAsync( Encoding.ASCII.GetBytes( "Hello, world." ) );
+        }
+    }
+}
